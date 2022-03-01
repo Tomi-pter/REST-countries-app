70140a6924cb53d7bfd2121e6ed1f9357a80513a
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2022-03-01 17:53:41 +0100

    added fallback for search query

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -76,34 +76,40 @@ function Countries() {
       </section>
 
       {searchParams.length > 0 ? (
-        <main className="grid grid-cols-1 gap-12 py-5 px-12 sm:px-24 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 2xl:container 2xl:mx-auto ">
-          {filtered.map(({ name, flags, population, region, capital }) => {
-            return (
-              <Link key={name.official} to={`/${capital}`}>
-                <section className="rounded-lg shadow-sm overflow-hidden bg-light-element dark:bg-dark-element text-light-text dark:text-dark-text">
-                  <div className="h-32">
-                    <img
-                      src={flags.svg}
-                      alt="country flag"
-                      className="object-cover h-32 w-full"
-                    />
-                  </div>
-                  <h2 className="font-NSSB py-4 px-4">{name.common}</h2>
-                  <p className="px-4">
-                    <span className="font-NSSB">Population:</span>{" "}
-                    {populationToLocale(population)}
-                  </p>
-                  <p className="px-4">
-                    <span className="font-NSSB">Region:</span> {region}
-                  </p>
-                  <p className="px-4 pb-4">
-                    <span className="font-NSSB">Capital:</span> {capital}
-                  </p>
-                </section>
-              </Link>
-            );
-          })}
-        </main>
+        filtered.length > 0 ? (
+          <main className="grid grid-cols-1 gap-12 py-5 px-12 sm:px-24 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 2xl:container 2xl:mx-auto ">
+            {filtered.map(({ name, flags, population, region, capital }) => {
+              return (
+                <Link key={name.official} to={`/${capital}`}>
+                  <section className="rounded-lg shadow-sm overflow-hidden bg-light-element dark:bg-dark-element text-light-text dark:text-dark-text">
+                    <div className="h-32">
+                      <img
+                        src={flags.svg}
+                        alt="country flag"
+                        className="object-cover h-32 w-full"
+                      />
+                    </div>
+                    <h2 className="font-NSSB py-4 px-4">{name.common}</h2>
+                    <p className="px-4">
+                      <span className="font-NSSB">Population:</span>{" "}
+                      {populationToLocale(population)}
+                    </p>
+                    <p className="px-4">
+                      <span className="font-NSSB">Region:</span> {region}
+                    </p>
+                    <p className="px-4 pb-4">
+                      <span className="font-NSSB">Capital:</span> {capital}
+                    </p>
+                  </section>
+                </Link>
+              );
+            })}
+          </main>
+        ) : (
+          <main className="flex justify-center">
+            <h2 className="uppercase">No countries match your search</h2>
+          </main>
+        )
       ) : (
         <main className="grid grid-cols-1 gap-12 py-5 px-12 sm:px-24 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 2xl:container 2xl:mx-auto ">
           {countries.map(({ name, flags, population, region, capital }) => {
