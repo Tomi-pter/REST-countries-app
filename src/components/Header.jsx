cb4d3874c05f68288021c92ba3356e3a38495b3e
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2022-03-01 17:40:12 +0100

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,7 +29,7 @@ function Header() {
                 d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
               />
             </svg>
-            <p className="pl-2 font-NSSB">Dark Mode</p>
+            <span className="pl-2 font-NSSB">Dark Mode</span>
           </button>
         </div>
       </header>
