7ab0d97c7abb9ce29d092397915b0621bd36244a
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2022-03-02 14:13:57 +0100

    changed input font size and color

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,9 +2,9 @@ import { searchIcon } from "./SearchIcon";
 
 function Search({ searchCountries, searchParams }) {
   return (
-    <div className="bg-light-bg dark:bg-dark-bg p-4 text-sm relative font-NSL">
+    <div className="bg-light-bg dark:bg-dark-bg p-4 text-lg relative font-NSL">
       <input
-        className="py-3 pl-14 pr-2 bg-light-element dark:bg-dark-element my-2 w-full rounded text-light-input dark:text-dark-text placeholder:text-light-text dark:placeholder:text-dark-text shadow-skin"
+        className="py-3 pl-14 pr-2 bg-light-element dark:bg-dark-element my-2 w-full rounded text-light-input dark:text-dark-text placeholder:text-light-input dark:placeholder:text-dark-text shadow-skin"
         type="search"
         name="search"
         placeholder="Search for any country..."
