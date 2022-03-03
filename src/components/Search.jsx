a8d0ae281f01e9a59dcf8dbde81f6475f46214b0
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2022-03-03 09:23:47 +0100

    search dark mode color

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,14 +4,14 @@ function Search({ searchCountries, searchParams }) {
   return (
     <div className="bg-light-bg dark:bg-dark-bg p-4 text-lg relative font-NSL">
       <input
-        className="py-3 pl-14 pr-2 bg-light-element dark:bg-dark-element my-2 w-full rounded text-light-input dark:text-red-500 placeholder:text-light-input dark:placeholder:text-dark-text shadow-skin"
+        className="py-3 pl-14 pr-2 bg-light-element dark:bg-dark-element my-2 w-full rounded text-light-input dark:text-yellow-300 placeholder:text-light-input dark:placeholder:text-yellow-300 shadow-skin"
         type="search"
         name="search"
         placeholder="Search for any country..."
         value={searchParams}
         onChange={(e) => searchCountries(e.target.value)}
       />
-      <span className="absolute left-8 top-10 text-light-text dark:text-dark-text">
+      <span className="absolute left-8 top-10 text-light-text dark:text-yellow-300">
         {searchIcon}
       </span>
     </div>
